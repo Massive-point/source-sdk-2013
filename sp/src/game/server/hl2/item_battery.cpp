53ef09af512423d552ea41e8b9eda9bba75da87b
Massive-point <64304213+Massive-point@users.noreply.github.com>
2020-10-18 12:25:37 +0300

    Update item_battery.cpp

--- sp/src/game/server/hl2/item_battery.cpp
+++ sp/src/game/server/hl2/item_battery.cpp
@@ -11,10 +11,17 @@
 #include "gamerules.h"
 #include "items.h"
 #include "engine/IEngineSound.h"
+//////////ADD/////////////////////////////////////////////////////////
+#include "dlight.h"
+#include "iefx.h"
+//////////ADD/////////////////////////////////////////////////////////
 
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
 
+//////////ADD/////////////////////////////////////////////////////////
+IVEfx *effects;
+//////////ADD/////////////////////////////////////////////////////////
 class CItemBattery : public CItem
 {
 public:
@@ -25,6 +32,22 @@ public:
 		Precache( );
 		SetModel( "models/items/battery.mdl" );
 		BaseClass::Spawn( );
+		//////////ADD/////////////////////////////////////////////////////////
+			Vector vAttachment, vAng;
+			QAngle angles;
+			GetAttachment( 1, vAttachment, angles );
+
+			AngleVectors( angles, &vAng );
+			vAttachment += vAng;
+		
+			dlight_t *dl = effects->CL_AllocDlight ( -1 );
+			dl->origin = vAttachment;
+			dl->color.r = 140;
+			dl->color.g = 180;
+			dl->color.b = 255;
+			dl->radius = random->RandomFloat( 140.0f, 156.0f ); // 245.0 256.0
+			dl->decay = 512.0f;
+		/////////ADD//////////////////////////////////////////////////////////
 	}
 	void Precache( void )
 	{
@@ -36,6 +59,10 @@ public:
 	bool MyTouch( CBasePlayer *pPlayer )
 	{
 		CHL2_Player *pHL2Player = dynamic_cast<CHL2_Player *>( pPlayer );
+		/////////ADD//////////////////////////////////////////////////////////
+			dlight_t *dl = effects->CL_AllocDlight ( -1 );
+		    dl->die;
+		/////////ADD//////////////////////////////////////////////////////////
 		return ( pHL2Player && pHL2Player->ApplyBattery() );
 	}
 };
